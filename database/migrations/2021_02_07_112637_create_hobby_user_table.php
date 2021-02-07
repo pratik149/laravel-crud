e556ffb1c9b3da6cef5ae3e8d5c9f3b36739b759
Pratik Rane <pratikrane149@gmail.com>
2021-02-07 17:01:35 +0530

    create migration for hobby_user bridge table

---
+++ database/migrations/2021_02_07_112637_create_hobby_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHobbyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hobby_user', function (Blueprint $table) {
+            $table->id();
+			$table->unsignedBigInteger('user_id');
+			$table->unsignedBigInteger('hobby_id');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hobby_user');
+    }
+}
